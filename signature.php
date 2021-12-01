456df817cfc3b9d013da31880d031c3616b7460e
nurulsyfiqah <52842608+nurulsyfiqah@users.noreply.github.com>
2021-12-01 14:42:35 +0800

    Update and rename s.php to signature.php

--- signature.php
+++ signature.php
@@ -72,8 +72,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script src="<?=ASSETS?>js/jquery-3.6.0.min.js"></script>
-    <script src="<?=ASSETS?>js/signature.js"></script>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script src="js/signature.js"></script>
 
     <script>
         $(document).ready(function(){ 
